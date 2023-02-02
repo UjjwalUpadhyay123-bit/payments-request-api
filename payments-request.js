9ef412ca72e9cd5f8360dbbb719e4bb19cc5877a
Ujjwal Upadhyay <ujjwalkool1990@gmail.com>
2023-02-02 14:43:39 +0530

    Update payment req - 4

--- payments-request.js
+++ payments-request.js
@@ -24,16 +24,104 @@ function validateResponse(response) {
   
 function load () {
     if (window.PaymentRequest) {
+        // const data = {
+        //   mobile: '',
+        //   initTxnData: {
+        //     mid: 'AliSub58582630351896',
+        //     orderId: 'integration_20230202133024',
+        //     amount: '1',
+        //     txnToken: 'a1158f03603c42c893b0bb823afbba981675324839092'
+        //   },
+        //   type: 'FULL_APP_INVOKE'
+        // };
         const data = {
-          mobile: '',
-          initTxnData: {
-            mid: 'AliSub58582630351896',
-            orderId: 'integration_20230202133024',
-            amount: '1',
-            txnToken: 'a1158f03603c42c893b0bb823afbba981675324839092'
-          },
-          type: 'FULL_APP_INVOKE'
-        };
+          "fetchPayOption": {
+              "body": {
+                  "merchantDetails": {
+                      "mcc": "5812",
+                      "merchantVpa": "siddharth-sawarkar@paytm",
+                      "merchantName": "XYZ Enterprises",
+                      "merchantDisplayName": "Paytm  Airtel Recharge",
+                      "merchantLogo": "https://stage-webapp.paytm.in//integration-app/blink-checkout/logo11.png",
+                      "isAppInvokeAllowed": true,
+                      "merchantBankName": "Kanishk Saini",
+                      "verifiedMerchant": true,
+                      "autoAppInvokeAllowed": false
+                  },
+                  "walletOnly": false,
+                  "nativeJsonRequestSupported": true,
+                  "paymentFlow": "NONE",
+                  "merchantPayOption": {
+                      "upiProfile": {
+                          "respDetails": {
+                              "profileDetail": {
+                                  "vpaDetails": [
+                                      {
+                                          "name": "6360350289@paytm",
+                                          "defaultCreditAccRefId": "331755472",
+                                          "defaultDebitAccRefId": "331755472",
+                                          "primary": true
+                                      }
+                                  ],
+                                  "bankAccounts": [
+                                      {
+                                          "bank": "ICICI Bank",
+                                          "ifsc": "ICIC0000388",
+                                          "accRefId": "331755472",
+                                          "maskedAccountNumber": "XXXXXXXX4953",
+                                          "accountType": "SAVINGS",
+                                          "credsAllowed": [
+                                              {
+                                                  "CredsAllowedDLength": "6",
+                                                  "CredsAllowedDType": "Numeric",
+                                                  "CredsAllowedSubType": "SMS",
+                                                  "CredsAllowedType": "OTP",
+                                                  "dLength": "6"
+                                              },
+                                              {
+                                                  "CredsAllowedDLength": "4",
+                                                  "CredsAllowedDType": "Numeric",
+                                                  "CredsAllowedSubType": "MPIN",
+                                                  "CredsAllowedType": "PIN",
+                                                  "dLength": "4"
+                                              }
+                                          ],
+                                          "name": "UJJWAL KUMAR UPADHYAY",
+                                          "mpinSet": "Y",
+                                          "txnAllowed": "ALL",
+                                          "branchAddress": "MUMBAI MALAD II",
+                                          "pgBankCode": "ICICI",
+                                          "bankMetaData": {
+                                              "perTxnLimit": "200000",
+                                              "bankHealth": {
+                                                  "category": "GREEN",
+                                                  "displayMsg": ""
+                                              }
+                                          },
+                                          "bankLogoUrl": "https://staticgw3.paytm.in/native/bank/ICICI.png",
+                                          "bankAccountFeatureList": [
+                                              "OTM",
+                                              "SUBSCRIPTION"
+                                          ]
+                                      }
+                                  ],
+                                  "profileStatus": "ACTIVE",
+                                  "upiLinkedMobileNumber": "916360350289",
+                                  "deviceBinded": false
+                              },
+                              "metaDetails": {
+                                  "npciHealthCategory": "GREEN",
+                                  "npciHealthMsg": ""
+                              }
+                          },
+                          "priority": "3",
+                          "upiOnboarding": false,
+                          "onboardNewUPIBankAcct": true
+                      }
+                  }
+              }
+          }
+      };
         const supportedPaymentMethods = [
           {supportedMethods: 'https://securegw.paytm.in/pay', data: data},
           // {
